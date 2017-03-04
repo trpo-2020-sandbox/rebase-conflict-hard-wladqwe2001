f798ffa1b29c4261710d9ac1a67ee0960d14d19e
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 17:43:05 +0700

    Extract printing array to function

--- main.c
+++ main.c
@@ -16,27 +16,23 @@ int max_element(int* arr, int size)
     return max;
 }
 
-int main()
+void print_array(int* arr, int size)
 {
-    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
-
-<<<<<<< HEAD
-    const int max = max_element(arr, 10);
-
     printf("Array: ");
     int i;
-    for (i = 0; i < 10; ++i) {
-=======
-    const int max = max_element(arr, N_ELEMENTS(arr));
-
-    printf("Array: ");
-    int i;
-    for (i = 0; i < N_ELEMENTS(arr); ++i) {
->>>>>>> Count array items with macro
+    for (i = 0; i < size; ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
 
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5};
+
+    const int max = max_element(arr, N_ELEMENTS(arr));
+
+    print_array(arr, N_ELEMENTS(arr));
     printf("Max element: %d\n", max);
 
     return 0;
